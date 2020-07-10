6aac10cc815572bf9096d3e12eea98a48f086b48
Mike Bannister <notimpossiblemike@gmail.com>
2020-07-10 16:19:36 -0400

    Cleanup

--- test.js
+++ test.js
@@ -1,15 +1,20 @@
 const puppeteer = require('puppeteer')
-const dedent = require('dedent')
 const { parseUrl } = require('url')
-const { exec } = require('shelljs')
+const { spawn } = require('child_process')
+
+const appUrl = 'https://react-redux.realworld.io'
+const apiUrl = 'https://conduit.productionready.io/api'
 
 const puppeteerConfig = { headless: false, devtools: true }
 
-const curlCommandForRequest = request =>
-  `
-    curl -v '${request.url()}' -H 'user-agent: ${
-    request.headers()['user-agent']
-  }' -H 'referer: ${request.headers()['referer']}'`
+const curlArgumentsForRequest = request => [
+  '-v',
+  request.url(),
+  '-H',
+  `user-agent: ${request.headers()['user-agent']}`,
+  '-H',
+  `referer: ${request.headers()['referer']}`,
+]
 
 const main = async () => {
   const browser = await puppeteer.launch(puppeteerConfig)
@@ -19,32 +24,44 @@ const main = async () => {
 
   page.on('request', request => {
     const url = request.url()
-    if (url.startsWith('https://conduit.productionready.io/api')) {
-      const curl = curlCommandForRequest(request)
-      const result = exec(curl)
-      const statusLine = result.stderr
-        .split('\n')
-        .find(line => line.startsWith('< status: '))
-      const status = statusLine && parseInt(statusLine.match(/(\d){3}/g)[0])
+    if (url.startsWith(apiUrl)) {
+      const curlArgs = curlArgumentsForRequest(request)
+      const curling = spawn('curl', curlArgs)
+      curling.stdout.pipe(process.stdout)
+      curling.stderr.pipe(process.stderr)
 
-      if (status) {
+      const standardOutput = []
+      curling.stdout.on('data', data => {
+        standardOutput.push(data)
+      })
+
+      const errorOutput = []
+      curling.stderr.on('data', data => {
+        errorOutput.push(data)
+      })
+
+      curling.on('close', data => {
+        const statusLine = errorOutput
+          .join('')
+          .split('\n')
+          .find(line => line.startsWith('< status: '))
+        const status = statusLine && parseInt(statusLine.match(/(\d){3}/g)[0])
+
         return request.respond({
           headers: {
             'access-control-allow-origin': '*',
           },
           contentType: 'application/json',
-          body: result,
+          body: standardOutput.join(''),
           status,
         })
-      }
+      })
+    } else {
+      request.continue()
     }
-    request.continue()
   })
 
-  await Promise.all([
-    page.waitForNavigation(),
-    page.goto('https://react-redux.realworld.io'),
-  ])
+  await Promise.all([page.waitForNavigation(), page.goto(appUrl)])
 
   const articleSelector = '.article-preview:first-child a'
 
